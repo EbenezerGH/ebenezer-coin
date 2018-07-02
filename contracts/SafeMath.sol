38de7b42f565ea3a186d464b50610b70daf2b9a9
Ebenezer Ackon <Ebenezer@younow.com>
2018-07-02 16:26:54 -0400

    fix Typo in docs

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
 // ----------------------------------------------------------------------------
-// Safe maths
+// Safe math
 // ----------------------------------------------------------------------------
 contract SafeMath {
     function safeAdd(uint a, uint b) public pure returns (uint c) {
